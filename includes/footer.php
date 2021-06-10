e271b6bfb810754849882960946f52a9748ec7d5
Xplod4432 <tushar4432@yahoo.com>
2021-06-10 21:29:31 +0530

    added footer and carousel

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<footer class="footer bg-foot">
+        <div class="lr-links text-white">
+        <ul class="footer-link">
+            <li><a href="">Contact Us</a></li>
+            <li><a href="">About Us</a></li>
+            <li><a href="">Contact Webmaster</a></li>
+        </ul>
+        </div> 
+    </footer>
+</body>
+</html>
--- includes/header.php
+++ includes/header.php
@@ -5,21 +5,45 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-
+  <link href="./css/local.css" rel="stylesheet">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 
-	<title>PHP - <?php echo $title ?></title>
+	<title><?php echo $title ?></title>
 </head>
 <body>
-	<div class="container">
-	<h1>Hello World</h1>
-	<ul class="nav">
-		<li class ="nav-item"><a class ="nav-link" href="index.php">Home</a></li>
-		<li class ="nav-item"><a class ="nav-link" href="array.php">Simple Array and Printouts</a></li>
-		<li class ="nav-item"><a class ="nav-link" href="forloop.php">Simple For Loop</a></li>
-		<li class ="nav-item"><a class ="nav-link" href="whileloop.php">Simple While/Do While Loop</a></li>
-		<li class ="nav-item"><a class ="nav-link" href="ifstatement.php">Simple If Statement</a></li>
-		<li class ="nav-item"><a class ="nav-link" href="switchstatement.php">Simple Switch Statement</a></li>
-		<li class ="nav-item"><a class ="nav-link" href="stringmanip.php">Simple String Manipulation</a></li>
-		<li class ="nav-item"><a class ="nav-link" href="datetimemanip.php">Date Time Manipulation</a></li>
-	</ul>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
