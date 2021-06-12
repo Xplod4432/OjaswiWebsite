7c411174033e931c52df97467082b1418584ff8d
me2527 <study.akm2002@outlook.com>
2021-06-12 22:47:02 +0530

    Modified

--- AboutOjaswi.php
+++ AboutOjaswi.php
@@ -1,3 +1,71 @@
+<html>
+
+<!-- Raven2 Example
+
+     Use fgets function to get lines from
+     a file one line at a time.  Then echo 
+     the string to the document in formatted 
+     form using pre tags.
+-->
+
+<head>
+<title>About Ojaswi</title>
+<style>
+body     { background-color: #FA8128;
+           color:            #600000;
+           margin: 50px 50px 50px 50px; }
+           font-size: 12pt;
+            }
+h1       { font-size: 300%; 
+           font-family: "Times New Roman"}
+h2       { font-family: Verdana;
+           font-size: 150%; }
+.prebody { font-family: Arial;
+           font-size: 90%;
+           margin: 25px 50px 75px 100px; }
+</style>
+</head>
+
+<body>
+<center>
+<h2>About Ojaswi</h2>
+
+<?php
+    $text = fopen("aboutOjaswi.txt", "rb");
+
+    // Output the poem heading.
+    $line = rtrim(fgets($text));
+    echo "<h1>" . $line . "</h1>";
+
+    // Output remainder of poem lines.
+    echo "<pre class=\"prebody\">\n";
+
+    while($line = fgets($text, 100))
+    {
+        echo $line;
+    }
+    echo "</pre>";
+?>
+
+<h1>Dignitories</h1>
 <?php 
-echo "Our Website"; 
-?> 
+$text1 = fopen("dignitories.txt", "rb");
+
+// Output the poem heading.
+$line = rtrim(fgets($text1));
+echo "<h1>" . $line . "</h1>";
+
+// Output remainder of poem lines.
+echo "<pre class=\"h2\">\n";
+
+
+while($line = fgets($text1, 50))
+{
+    echo $line;
+}
+echo "</pre>";
+?>
+</center>
+
+</body>
+</html>
