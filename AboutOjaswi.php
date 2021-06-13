0daef28868536c33d46ef45583e14c536c7abb7e
me2527 <study.akm2002@outlook.com>
2021-06-13 18:58:35 +0530

    Changes made in dignit, in php of reading a file

--- AboutOjaswi.php
+++ AboutOjaswi.php
@@ -5,7 +5,7 @@
 
 ?>
 <center>
-<h2>About Ojaswi</h2>
+<h1>About Ojaswi</h1>
 
 <?php
     $text = fopen("aboutOjaswi.txt", "rb");
@@ -24,31 +24,48 @@
     echo "</pre>";
 ?>
 
-<h1>Dignitories</h1>
+<h2>Dignitories</h2>
 <?php 
 $text1 = fopen("dignitories.txt", "rb");
 
 // Output the text heading.
 $line = rtrim(fgets($text1));
-echo "<h1>" . $line . "</h1>";
+echo "<h4>" . $line . "</h4>";
 
 // Output remainder of text lines.
-echo "<pre class=\"h2\">\n";
+echo "<pre class=\"prebody\">\n";
 
 
-while($line = fgets($text1, 50))
+while($line = fgets($text1, 200))
 {
     echo $line;
 }
 echo "</pre>";
-?>
 
+
+// Output the text heading.
+$line = rtrim(fgets($text1));
+echo "<h3>" . $line . "</h3>";
+
+// Output remainder of text lines.
+echo "<pre class=\"prebody\">\n";
+
+
+while($line = fgets($text1, 300))
+{
+    echo $line;
+}
+echo "</pre>";
+
+
+?>
+<centre>
 <button type="button" id="button" class="btn btn-info btn-lg"><i class="bi-geo-alt-fill" style="font-size:1rem;"></i>&nbspLocate Us</button>
 <br/>
 <br/>
 
 <div id="iframeHolder"></div>
-</center>
+</centre>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js"></script>
 <script type="text/javascript">
 $(function(){
