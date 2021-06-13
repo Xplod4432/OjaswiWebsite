9bd2cfe968fe4002851bd32f0d597be16bad2691
me2527 <study.akm2002@outlook.com>
2021-06-13 17:32:00 +0530

    updated map, txt, margins

--- AboutOjaswi.php
+++ AboutOjaswi.php
@@ -1,7 +1,8 @@
 <?php 
     $title = "About Ojaswi";
     $extracss = "./css/about.css";
-    require './includes/header.php' 
+    require './includes/header.php' ;
+
 ?>
 <center>
 <h2>About Ojaswi</h2>
@@ -41,9 +42,11 @@ while($line = fgets($text1, 50))
 }
 echo "</pre>";
 ?>
+
 <button type="button" id="button" class="btn btn-info btn-lg"><i class="bi-geo-alt-fill" style="font-size:1rem;"></i>&nbspLocate Us</button>
 <br/>
 <br/>
+
 <div id="iframeHolder"></div>
 </center>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js"></script>
@@ -51,11 +54,11 @@ echo "</pre>";
 $(function(){
     $('#button').click(function(){ 
         if(!$('#iframe').length) {
-                $('#iframeHolder').html('<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3410.726564801247!2d75.70295481498906!3d31.255991981457804!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391a5f5e9c489cf3%3A0x4049a5409d53c300!2sLovely%20Professional%20University!5e0!3m2!1sen!2sin!4v1623524672797!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>');
+                $('#iframeHolder').html('<iframe src="https://www.google.com/maps/d/embed?mid=1fOWrMPRrasEddZTtDvLXWmse3Fw" width="640" height="480"></iframe>');
         }
     });   
 });
 </script>
 <?php
-include "./includes/footer.php"
+require "./includes/footer.php"
 ?>
