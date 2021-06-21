b3093bd722b2e46adbb73545a8fce0b57b4b6b08
Xplod4432 <tushar4432@yahoo.com>
2021-06-21 22:02:11 +0530

    Added accordion to about page

--- AboutOjaswi.php
+++ AboutOjaswi.php
@@ -19,24 +19,34 @@
 ?>
 </p>
 <h2>Dignitories</h2>
-<?php 
+<div class="accordion" id="accordionExample">
+<?php
     $text1 = fopen("dignitories.txt", "rb");
-
+    $i = 0;
     while (!feof($text1)) {
         $line=fgets($text1);
-        echo "<h3>$line<h3>";
+        echo "<div class='accordion-item'>
+        <h2 class='accordion-header' id='heading$i'>
+          <button class='accordion-button' type='button' data-bs-toggle='collapse' data-bs-target='#collapse$i' aria-expanded='true' aria-controls='collapse$i'>$line </button>
+          </h2>
+          <div id='collapse$i' class='accordion-collapse collapse show' aria-labelledby='heading$i' data-bs-parent='#accordionExample'>
+            <div class='accordion-body'>";
         $desc=fgets($text1);
-        echo "<p>$desc</p>";
+        echo "$desc 
+        </div>
+        </div>
+      </div>";
+      $i++;
     }
     fclose($text1);
 ?>
-<centre>
+</div>
 <button type="button" id="button" class="btn btn-info btn-lg"><i class="bi-geo-alt-fill" style="font-size:1rem;"></i>&nbspLocate Us</button>
 <br/>
 <br/>
 
 <div id="iframeHolder"></div>
-</centre>
+</center>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js"></script>
 <script type="text/javascript">
 $(function(){
