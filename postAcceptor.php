72d02e50779d56a3206b2c43b5282c1450f928c4
Tushar Vilekar <73468854+Xplod4432@users.noreply.github.com>
2021-08-01 00:14:14 +0530

    Update postAcceptor.php

--- postAcceptor.php
+++ postAcceptor.php
@@ -1,30 +1,4 @@
 <?php
-  /***************************************************
-   * Only these origins are allowed to upload images *
-   ***************************************************/
-  $accepted_origins = array("http://localhost", "http://192.168.1.1");
-
-  /*********************************************
-   * Change this line to set the upload folder *
-   *********************************************/
-  $imageFolder = "images/";
-
-  if (isset($_SERVER['HTTP_ORIGIN'])) {
-    // same-origin requests won't set an origin. If the origin is set, it must be valid.
-    if (in_array($_SERVER['HTTP_ORIGIN'], $accepted_origins)) {
-      header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
-    } else {
-      header("HTTP/1.1 403 Origin Denied");
-      return;
-    }
-  }
-
-  // Don't attempt to process the upload on an OPTIONS request
-  if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    header("Access-Control-Allow-Methods: POST, OPTIONS");
-    return;
-  }
-
   reset($_FILES);
 $temp = current($_FILES);
 
