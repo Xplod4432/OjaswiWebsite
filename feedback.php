a3370307def729a6f988a583537ae4c203d52c3c
Tushar Vilekar <tushar4432@yahoo.com>
2021-08-02 01:00:17 +0530

    magazine

--- feedback.php
+++ feedback.php
@@ -11,13 +11,23 @@
         $ts = date('Y-m-d H:i:s');
         $appr = $_POST['rating'];
         $feedText = $_POST['feedText'];
-        echo $experience.$fren.$ts.$appr.$feedText;
 
         //Call function to insert and track if success or not
         $isSuccess = $crud->insertFeedbacks($experience,$fren,$ts,$appr,$feedText);
         if($isSuccess)
-        {
-            include 'includes/successmessage.php';
+        { ?>
+            <div class="jumbotron text-center">
+  <h1 class="display-3">Thank You!</h1>
+  <p class="lead"><strong>Please check your email</strong> for further instructions on how to complete your account setup.</p>
+  <hr>
+  <p>
+    Having trouble? <a href="">Contact us</a>
+  </p>
+  <p class="lead">
+    <a class="btn btn-primary btn-sm" href="https://bootstrapcreative.com/" role="button">Continue to homepage</a>
+  </p>
+</div>
+           <?php include 'includes/successmessage.php';
         }
         else
         {
